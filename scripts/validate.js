850a6ca465d63fd92f2c38cf9c93464a7dd3ed33
Ilya Krasnitskiy <ikrasnickij@gmail.com>
2022-12-23 06:41:05 +0300

    modified:   scripts/validate.js

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,30 +1,33 @@
-const showInputError = (formElement, inputElement, errorMessage , config) => {
+const showInputError = (formElement , inputElement , errorMessage , config) => {
   const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
   inputElement.classList.add(config.inputErrorClass);
   errorElement.textContent = errorMessage;
 };
 
-const hideInputError = (formElement, inputElement, config) => {
+const hideInputError = (formElement , inputElement , config) => {
   const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
   inputElement.classList.remove(config.inputErrorClass);
   errorElement.textContent = '';
 };
 
-const checkInputValidity = (formElement, inputElement , config) => {
+const checkInputValidity = (formElement , inputElement , config) => {
   if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage, config);
-    toggleButtonDisable(formElement , config);
+    showInputError(formElement , inputElement , inputElement.validationMessage , config);
   } else {
-    hideInputError(formElement, inputElement , config);
-    toggleButtonEnable(formElement , config);
+    hideInputError(formElement , inputElement , config);
   }
 };
 
-const setEventListeners = (formElement, config) => {
+const setEventListeners = (formElement , config) => {
   const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
+  const button = formElement.querySelector(config.submitButtonSelector);
+
+  toggleButtonDisable(inputList , button , config);
+
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', function () {
       checkInputValidity(formElement, inputElement , config);
+      toggleButtonDisable(inputList , button , config)
     });
   });
 };
@@ -36,13 +39,24 @@ const enableValidation = (config) => {
   });
 };
 
-const toggleButtonDisable = (formElement , config) => {
-  formElement.querySelector(config.submitButtonSelector).classList.add(config.inactiveButtonClass);
-}
 
-const toggleButtonEnable = (formElement , config) => {
-  formElement.querySelector(config.submitButtonSelector).classList.remove(config.inactiveButtonClass);
+const toggleButtonDisable = (inputList , button , config) => {
+  const isFormValid = inputList.every(inputElement => inputElement.validity.valid)
+  if (isFormValid) {
+    button.classList.remove(config.inactiveButtonClass);
+    button.disabled = false;
+  } else {
+    button.classList.add(config.inactiveButtonClass);
+    button.disabled = true;
+  }
 }
+// const toggleButtonDisable = (formElement , config) => {
+//   formElement.querySelector(config.submitButtonSelector).classList.add(config.inactiveButtonClass);
+// }
+
+// const toggleButtonEnable = (formElement , config) => {
+//   formElement.querySelector(config.submitButtonSelector).classList.remove(config.inactiveButtonClass);
+// }
 
 enableValidation({
   formSelector: '.popup__form',
